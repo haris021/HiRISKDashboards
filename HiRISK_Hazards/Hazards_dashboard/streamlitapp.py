a2ec2dd7aa84e18e995fb7a900071ebcde77ac3a
haris021 <harismushtaq21@yahoo.com>
2024-03-26 09:57:56 +0500

    Update streamlitapp.py

--- HiRISK_Hazards/Hazards_dashboard/streamlitapp.py
+++ HiRISK_Hazards/Hazards_dashboard/streamlitapp.py
@@ -19,7 +19,7 @@ px.set_mapbox_access_token(mapbox_access_token)
 st. set_page_config(layout="wide")
 st.title("High Mountain Hazard Data")
 st.divider()
-df_avalanches = pd.read_csv("./Avalanches/HiAVALDB.csv", encoding = "latin1")
+df_avalanches = pd.read_csv("HiRISK_Hazards/Avalanches/HiAVALDB.csv", encoding = "latin1")
 df_avalanches = df_avalanches.drop(["Unnamed: 0"], axis=1)
 
 df_glofs = pd.read_csv("../GLOFs/HMAGLOFDB.csv", encoding = "latin1")
